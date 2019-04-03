7bfb666d2c30fc65746712a92ff433f4eb345036
pedrobell1 <busybell@Live.com>
2019-04-03 17:53:06 -0400

    Add files via upload

---
+++ head first projects/src/bottles99.java
@@ -0,0 +1,19 @@
+
+public class bottles99 {
+
+	public static void main(String[] args) {
+	int beer= 99;
+			while(beer!=0){
+				System.out.println(beer+" bottles of beer on the wall,"+" "+ beer+" " +"bottles of beer!");
+				beer--;
+				System.out.println("take one down pass it around,"+beer +"bottles of beer on the wall!");
+			}
+				if(beer<=0){
+					System.out.print("go get me another beer bobby");
+				
+			}
+
+
+	}
+
+}
